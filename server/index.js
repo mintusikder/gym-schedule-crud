e8d682687e9f176cc8bd1f9e8cd0a67a7f61e80d
Mintu Sikder <mintusikder15@gmail.com>
2025-05-17 12:23:33 +0600

    gym schedule

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors');
+require("dotenv").config()
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
