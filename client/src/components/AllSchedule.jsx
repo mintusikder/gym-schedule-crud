7064e8e85d0d2c821005d35ee15e876c7280f3c6
Mintu Sikder <mintusikder15@gmail.com>
2025-05-17 14:15:44 +0600

    added delete schedule

--- client/src/components/AllSchedule.jsx
+++ client/src/components/AllSchedule.jsx
@@ -2,10 +2,40 @@ import React, { useState } from "react";
 import { useLoaderData } from "react-router";
 import { MdDelete } from "react-icons/md";
 import { FaRegEdit } from "react-icons/fa";
+import Swal from "sweetalert2";
 const AllSchedule = () => {
   const initialData = useLoaderData();
   const [gyms, setGyms] = useState(initialData);
-
+  console.log(gyms);
+  const handelScheduleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/schedule/${id}`, {
+          method: "delete",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your schedule has been deleted.",
+                icon: "success",
+              });
+              const remaining = gyms.filter((gym) => gym._id !== id);
+              setGyms(remaining);
+            }
+          });
+      }
+    });
+  };
   return (
     <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
       <table className="table">
@@ -20,7 +50,7 @@ const AllSchedule = () => {
             <th>Actions</th>
           </tr>
         </thead>
-        
+
         {/* Table Body */}
         <tbody>
           {gyms.map((gym, index) => (
@@ -32,9 +62,19 @@ const AllSchedule = () => {
               <td>{gym.day}</td>
               <td>
                 <div className="join space-x-2">
-                  <button className="btn btn-sm"><FaRegEdit size={20} /></button>
-                  <button className="btn btn-sm btn-error"><MdDelete size={20} /></button>
-                  <button className="btn btn-sm btn-primary">Update</button>
+                  <button
+                    className="btn btn-sm btn-ghost hover:btn-info"
+                    aria-label="Edit schedule"
+                  >
+                    <FaRegEdit size={18} />
+                  </button>
+                  <button
+                    onClick={() => handelScheduleDelete(gym._id)}
+                    className="btn btn-sm btn-ghost hover:btn-error"
+                    aria-label="Delete schedule"
+                  >
+                    <MdDelete size={18} />
+                  </button>
                 </div>
               </td>
             </tr>
--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -29,6 +29,12 @@ async function run() {
     });
     app.get("/schedule", async (req, res) => {
       const result = await gymCollection.find().toArray();
+      res.send(result);
+    });
+    app.delete("/schedule/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await gymCollection.deleteOne(query);
       res.send(result);
     });
     console.log(
